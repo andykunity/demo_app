8d4594d52677b9c3fb16542309c090114ca5e23c
andykunity <andyk@unity3d.com>
2014-04-10 12:03:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6dc43c6c949128d175c39e91823a1e600602fd31843af79b5c34bba204fe1238d40df04467c6cdeb103984e0890532ce38368a2badc39bd80e0784e6e43a7839'
